b208b9b16d9a660cc2a57080e096c02c8db555c6
thoenis <sthoeni@kof.ethz.ch>
2019-12-01 08:45:04 +0100

    Tinker with day 1

--- R/day1.R
+++ R/day1.R
@@ -6,15 +6,30 @@ day1 <- function(input_path) {
 
   get_fuel_for_fuel <- function(fuel_masses) {
     out <- numeric(length(fuel_masses))
-    while(any((fuel_masses <- (floor(fuel_masses/3)-2)) > 0)) {
-      fuel_masses[fuel_masses < 0] <- 0
-      out <- out + fuel_masses
+    residual_fuel <- numeric(length(fuel_masses))
+
+    while(TRUE) {
+      fm <- floor(fuel_masses/3) - 2
+      residual_fuel[fm <= 0 & residual_fuel == 0] <- fuel_masses[fm <= 0 & residual_fuel == 0]
+      fm[fm < 0] <- 0
+      fuel_masses <- fm
+      out <- out + fm
+
+      if(all(fm <= 0)) {
+        break;
+      }
     }
-    out
+
+    list(
+      fuel_required = out,
+      residual_mass = residual_fuel
+    )
   }
 
-  fuel_for_fuel <- sum(get_fuel_for_fuel(fuel_per_module))
+  fuel_for_fuel_per_module <- get_fuel_for_fuel(fuel_per_module)
+  fuel_for_fuel <- sum(fuel_for_fuel_per_module$fuel_required)
+  fuel_to_wish_for <- sum(fuel_for_fuel_per_module$residual_mass)
 
   message(sprintf("The total fuel required after taking fuel mass into account is %s!", fuel_for_fuel + fuel_required))
-
+  message(sprintf("You need to wish really hard for another %d mass...", fuel_to_wish_for))
 }
