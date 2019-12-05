e7b5d8b18926195583f4546039c980ec200b466f
thoenis <sthoeni@kof.ethz.ch>
2019-12-05 08:29:50 +0100

    Add opcodes 3 and 4
    And now the trainride draws to an end...

--- R/IntCodeComputer.R
+++ R/IntCodeComputer.R
@@ -1,6 +1,12 @@
+# TODO:
+# * document these
+# * improve debugging (i.e. print current instruction etc.)
+
 runIntCodeComputer <- function(tape,
                                noun = NULL,
                                verb = NULL,
+                               stdin = function(){0},
+                               stdout = function(){},
                                pos = 0,
                                debug = 0) {
   if(!is.null(noun)) {
@@ -20,7 +26,7 @@ runIntCodeComputer <- function(tape,
 
   # Because fun ;P
   while({
-    state <- advanceState(state)
+    state <- advanceState(state, stdin, stdout)
 
     if(debug > 1) {
       print(state)
@@ -33,25 +39,39 @@ runIntCodeComputer <- function(tape,
   state$tape
 }
 
-advanceState <- function(state) {
+advanceState <- function(state,
+                         stdin,
+                         stdout) {
   tape <- state$tape
   pos <- state$pos
   debug <- state$debug
 
+  # Let's assume there are no more than 3 parameters
+  # Easy to extend anyway
+  nParams <- 3
+  instruction <- tapeGet(tape, pos)
+  opcode <- instruction %% 100
+  paramModes <- ((instruction %/% 100) %% 10^(nParams:1)) %/% 10^((nParams-1):0)
+
+
   if(debug > 0) {
-    message(sprintf("Executing opCode %d", tapeGet(tape, pos)))
+    message(sprintf("Got instruction %d", instruction))
+    message(sprintf("Executing opCode %d", opcode))
   }
 
   out <- switch(
     # Oh R, you silly stupid language, you...
     # Cast the opcode as character because otherwise multi-digit codes won't work
-    as.character(tapeGet(tape, pos)),
+    as.character(opcode),
     "1" = {
       list(
         tape = tapeSet(
           tape,
           tapeGet(tape, pos + 3),
-          tapeGet(tape, tapeGet(tape, pos + 1)) + tapeGet(tape, tapeGet(tape, pos + 2))),
+          tapeGet(tape,
+                  tapeGet(tape, pos + 1, paramModes[nParams - 2])) +
+            tapeGet(tape,
+                    tapeGet(tape, pos + 2, paramModes[nParams - 1]))),
         pos = pos + 4,
         halt = FALSE
       )
@@ -61,10 +81,28 @@ advanceState <- function(state) {
         tape = tapeSet(
           tape,
           tapeGet(tape, pos + 3),
-          tapeGet(tape, tapeGet(tape, pos + 1)) * tapeGet(tape, tapeGet(tape, pos + 2))),
+          tapeGet(tape,
+                  tapeGet(tape, pos + 1, paramModes[nParams - 2])) *
+            tapeGet(tape,
+                    tapeGet(tape, pos + 2, paramModes[nParams - 1]))),
         pos = pos + 4,
         halt = FALSE
       )
+    },
+    "3" = {
+      list(
+        tape = tapeSet(
+          tape,
+          tapeGet(tape, pos + 1, paramModes[nParams - 1]),
+          stdin()), # I'm giggling like a stupid person here ^^
+        pos = pos + 2,
+        halt = FALSE
+      )
+    },
+    "4" = {
+      stdout(tapeGet(tape, pos + 1, paramModes[nParams - 1]))
+      state$pos <- pos + 2
+      state
     },
     "99" = {
       list(
@@ -89,11 +127,16 @@ advanceState <- function(state) {
 #' Get the value stored on tape at the given address
 #'
 #' @param tape
-#' @param address
+#' @param parameter
+#' @param mode
 #'
 #' @return The value at address on tape
-tapeGet <- function(tape, address) {
-  tape[address + 1]
+tapeGet <- function(tape, parameter, mode = 0) {
+  if(mode == 0) {
+    tape[parameter + 1]
+  } else {
+    parameter
+  }
 }
 
 #' Store value on a tape
