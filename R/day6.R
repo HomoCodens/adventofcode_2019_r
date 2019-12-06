245ea4e123614c6e9d20a879f39f141df225fb4b
thoenis <sthoeni@kof.ethz.ch>
2019-12-06 09:06:48 +0100

    do d6p1 proper like
    least I did *some* tree traversing

--- R/day6.R
+++ R/day6.R
@@ -22,19 +22,16 @@ day6 <- function(path) {
   # For the first time in forever, there'll be music, there'll be light... *sing*
   COM <- nodes[["COM"]]
 
-  # COM$Prune(pruneFun = function(x){Distance(COM, x) < 9})
-  #
-  # COM$Do(function(n) {
-  #   n$nOrbits <- n$level
-  # })
+  # data.tree is not so smart after all
+  getNOrbits <- function(n) {
+    agg <- 0
+    if(length(n$children) > 0) {
+      agg <- sum(sapply(n$children, getNOrbits))
+    }
+    agg + n$level - 1
+  }
 
-  message(sprintf("There are %d orbits in total.", sum(ToDataFrameTree(COM, "level")$level - 1)))
+  message(sprintf("There are %d orbits in total.", getNOrbits(COM)))
 
   message(sprintf("To get to Santa we need %d orbital transfers...", Distance(FindNode(COM, "SAN"), FindNode(COM, "YOU")) - 2))
-
-  # COM$Do(function(n) {
-  #   n$nOrbits <- Cumulate(n, "nOrbits", sum)
-  # }, traversal = "post-order")
-  #
-  # print(COM, "nOrbits")
 }
