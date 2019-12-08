b104f8be020d156dfd173c652502f770dc7f445a
thoenis <sthoeni@kof.ethz.ch>
2019-12-08 13:37:31 +0100

    make day 5 ICC compatible

--- R/day5.R
+++ R/day5.R
@@ -1,17 +1,9 @@
 day5 <- function(path, debug = 0) {
   tape <- as.numeric(strsplit(readLines(path), ",")[[1]])
 
-  iccin <- function() {
-    1
-  }
-
-  iccout <- function(x) {
-    message(x)
-  }
-
   message("part 1")
-  runIntCodeComputer(tape, iccin = iccin, iccout = iccout, debug = debug)
+  runIntCodeComputer(tape, iccin = iccInput(1), iccout = message, debug = debug)
 
   message("\npart 2")
-  runIntCodeComputer(tape, iccin = function(){5}, iccout = iccout, debug  = debug)
+  runIntCodeComputer(tape, iccin = iccInput(5), iccout = message, debug  = debug)
 }
