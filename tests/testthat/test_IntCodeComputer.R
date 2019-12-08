81477ac78587d9648fd6eed2d737adc0739c4dd5
thoenis <sthoeni@kof.ethz.ch>
2019-12-08 13:42:46 +0100

    fix tests

--- tests/testthat/test_IntCodeComputer.R
+++ tests/testthat/test_IntCodeComputer.R
@@ -59,40 +59,40 @@ test_that("it prints", {
 
 context("IntCodeComputer")
 
-test_output_is <- function(expected) {
+test_arg_is <- function(expected) {
   function(actual) {
     expect_equal(actual, expected)
   }
 }
 
 test_that("day 2 example programs", {
-  out1 <- runIntCodeComputer(c(1,9,10,3,2,3,11,0,99,30,40,50))
-  expect_equal(out1, c(3500,9,10,70,
-                       2,3,11,0,
-                       99,
-                       30,40,50))
+  runIntCodeComputer(c(1,9,10,3,2,3,11,0,99,30,40,50),
+                     done = test_arg_is(c(3500,9,10,70,
+                                                  2,3,11,0,
+                                                  99,
+                                                  30,40,50)))
 
-  out2 <- runIntCodeComputer(c(1,0,0,0,99))
-  expect_equal(out2, c(2,0,0,0,99))
+  runIntCodeComputer(c(1,0,0,0,99),
+                     done = test_arg_is(c(2,0,0,0,99)))
 
-  out3 <- runIntCodeComputer(c(2,3,0,3,99))
-  expect_equal(out3, c(2,3,0,6,99))
+  runIntCodeComputer(c(2,3,0,3,99),
+                     done = test_arg_is(c(2,3,0,6,99)))
 
-  out4 <- runIntCodeComputer(c(2,4,4,5,99,0))
-  expect_equal(out4, c(2,4,4,5,99,9801))
+  runIntCodeComputer(c(2,4,4,5,99,0),
+                     done = test_arg_is(c(2,4,4,5,99,9801)))
 
-  out5 <- runIntCodeComputer(c(1,1,1,4,99,5,6,0,99))
-  expect_equal(out5, c(30,1,1,4,2,5,6,0,99))
+  runIntCodeComputer(c(1,1,1,4,99,5,6,0,99),
+                     done = test_arg_is(c(30,1,1,4,2,5,6,0,99)))
 })
 
 test_that("param modes", {
-  out1 <- runIntCodeComputer(c(1002,4,3,4,33))
-  expect_equal(out1, c(1002,4,3,4,99))
+  out1 <- runIntCodeComputer(c(1002,4,3,4,33),
+                             done = test_arg_is(c(1002,4,3,4,99)))
 })
 
 test_that("negative integers", {
-  out1 <- runIntCodeComputer(c(1101,100,-1,4,0))
-  expect_equal(out1, c(1101, 100, -1, 4, 99))
+  out1 <- runIntCodeComputer(c(1101,100,-1,4,0),
+                             done = test_arg_is(c(1101,100,-1,4,99)))
 })
 
 test_that("I/O", {
@@ -112,31 +112,31 @@ test_that("comparators", {
   # output 1 (if it is) or 0 (if it is not).
   prog1 <- c(3,9,8,9,10,9,4,9,99,-1,8)
 
-  runIntCodeComputer(prog1, iccin = iccInput(7), iccout = test_output_is(0))
-  runIntCodeComputer(prog1, iccin = iccInput(8), iccout = test_output_is(1))
+  runIntCodeComputer(prog1, iccin = iccInput(7), iccout = test_arg_is(0))
+  runIntCodeComputer(prog1, iccin = iccInput(8), iccout = test_arg_is(1))
 
   # Using position mode, consider whether the input is less than 8;
   # output 1 (if it is) or 0 (if it is not).
   prog2 <- c(3,9,7,9,10,9,4,9,99,-1,8)
 
-  runIntCodeComputer(prog2, iccin = iccInput(7), iccout = test_output_is(1))
-  runIntCodeComputer(prog2, iccin = iccInput(8), iccout = test_output_is(0))
-  runIntCodeComputer(prog2, iccin = iccInput(9), iccout = test_output_is(0))
+  runIntCodeComputer(prog2, iccin = iccInput(7), iccout = test_arg_is(1))
+  runIntCodeComputer(prog2, iccin = iccInput(8), iccout = test_arg_is(0))
+  runIntCodeComputer(prog2, iccin = iccInput(9), iccout = test_arg_is(0))
 
   # Using immediate mode, consider whether the input is equal to 8;
   # output 1 (if it is) or 0 (if it is not).
   prog3 <- c(3,3,1108,-1,8,3,4,3,99)
 
-  runIntCodeComputer(prog3, iccin = iccInput(7), iccout = test_output_is(0))
-  runIntCodeComputer(prog3, iccin = iccInput(8), iccout = test_output_is(1))
+  runIntCodeComputer(prog3, iccin = iccInput(7), iccout = test_arg_is(0))
+  runIntCodeComputer(prog3, iccin = iccInput(8), iccout = test_arg_is(1))
 
   # Using position mode, consider whether the input is less than 8;
   # output 1 (if it is) or 0 (if it is not).
   prog4 <- c(3,9,7,9,10,9,4,9,99,-1,8)
 
-  runIntCodeComputer(prog4, iccin = iccInput(7), iccout = test_output_is(1))
-  runIntCodeComputer(prog4, iccin = iccInput(8), iccout = test_output_is(0))
-  runIntCodeComputer(prog4, iccin = iccInput(9), iccout = test_output_is(0))
+  runIntCodeComputer(prog4, iccin = iccInput(7), iccout = test_arg_is(1))
+  runIntCodeComputer(prog4, iccin = iccInput(8), iccout = test_arg_is(0))
+  runIntCodeComputer(prog4, iccin = iccInput(9), iccout = test_arg_is(0))
 })
 
 test_that("jumps", {
@@ -145,13 +145,13 @@ test_that("jumps", {
 
   prog1 <- c(3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9) # (using position mode)
 
-  runIntCodeComputer(prog1, iccin = iccInput(0), iccout = test_output_is(0))
-  runIntCodeComputer(prog1, iccin = iccInput(1337), iccout = test_output_is(1))
+  runIntCodeComputer(prog1, iccin = iccInput(0), iccout = test_arg_is(0))
+  runIntCodeComputer(prog1, iccin = iccInput(1337), iccout = test_arg_is(1))
 
   prog2 <- c(3,3,1105,-1,9,1101,0,0,12,4,12,99,1) # (using immediate mode)
 
-  runIntCodeComputer(prog2, iccin = iccInput(0), iccout = test_output_is(0))
-  runIntCodeComputer(prog2, iccin = iccInput(1337), iccout = test_output_is(1))
+  runIntCodeComputer(prog2, iccin = iccInput(0), iccout = test_arg_is(0))
+  runIntCodeComputer(prog2, iccin = iccInput(1337), iccout = test_arg_is(1))
 })
 
 test_that("day 6", {
@@ -159,7 +159,7 @@ test_that("day 6", {
             1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
             999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99)
 
-  runIntCodeComputer(prog, iccin = iccInput(7), iccout = test_output_is(999))
-  runIntCodeComputer(prog, iccin = iccInput(8), iccout = test_output_is(1000))
-  runIntCodeComputer(prog, iccin = iccInput(9), iccout = test_output_is(1001))
+  runIntCodeComputer(prog, iccin = iccInput(7), iccout = test_arg_is(999))
+  runIntCodeComputer(prog, iccin = iccInput(8), iccout = test_arg_is(1000))
+  runIntCodeComputer(prog, iccin = iccInput(9), iccout = test_arg_is(1001))
 })
