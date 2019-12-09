d63a15d8bb57602201fd7442cbf7e23eff164fff
thoenis <sthoeni@kof.ethz.ch>
2019-12-09 09:47:59 +0100

    solve day 9 part 1

---
+++ R/day9.R
@@ -0,0 +1,8 @@
+day9 <- function(path) {
+  tape <- as.numeric(strsplit(readLines(path), ",")[[1]])
+
+  in1 <- iccInput(1, TRUE)
+  out1 <- message
+
+  runIntCodeComputer(tape, iccin = in1, iccout = out1)
+}
